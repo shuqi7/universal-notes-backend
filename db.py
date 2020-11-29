eb8f6212e5c07433eb96d972555cb37f2bdada14
shuqi7 <shuqi7sq@gmail.com>
2020-11-28 20:59:44 -0500

    Add identifier

--- db.py
+++ db.py
@@ -9,15 +9,20 @@ notes_table = db.notes
 
 
 def get_notes():
-      cursor = notes_table.find({})
+    cursor = notes_table.find({})
 
-      notes = []
+    notes = []
 
-      for document in cursor:
-            title = document["title"]
-            content = document["content"]
-            note = {"title": title, "content": content}
-            notes.append(note)
+    for document in cursor:
+        title = document["title"]
+        content = document["content"]
+        object_id = document["_id"]
+        identifier = str(object_id)
+        note = {
+            "id": identifier,
+            "title": title,
+            "content": content
+        }
+        notes.append(note)
 
-      return notes
-
+    return notes
